7d1c5f34648d676e9e88744acc0c031098335f72
Matheswaran K <mathes.btech@gmail.com>
2023-06-02 10:08:40 -0700

    ui changes

--- js/background.js
+++ js/background.js
@@ -1,95 +1,123 @@
-console.log('Background js loaded..')
+console.log("Background js loaded..");
 let CONFIG = {};
 CONFIG.RECORD_TIMER = 1.5 * 1000;
 
-const delay = ms => new Promise(res => setTimeout(res, ms));
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
 chrome.webRequest.onBeforeRequest.addListener(
-  function(request) {
+  function (request) {
     // https://cdn.evgnet.com/beacon/adpinc/prod/scripts/evergage.min.js
-    if(request.method == 'GET' 
-      && request.url.indexOf('evergage.com/api2/event/') > -1) 
-    {
-      console.log('GET Request ' , request)
+    if (
+      request.method == "GET" &&
+      request.url.indexOf("evergage.com/api2/event/") > -1
+    ) {
+      console.log("GET Request ", request);
       var url = new URL(decodeURIComponent(request.url));
-      var encodedString = url.search.replace('?event=','')
+      var encodedString = url.search.replace("?event=", "");
       var decodedString = atob(encodedString);
       var decodedJson = JSON.parse(decodedString);
       var epocheDate = Date.now().toString();
       var payload = decodedJson;
       var hostName = url.hostname;
-      hostName = hostName.split('.')[0];
-      var datasetName = url.pathname.split('/')[url.pathname.split('/').length-1]; 
+      hostName = hostName.split(".")[0];
+      var datasetName =
+        url.pathname.split("/")[url.pathname.split("/").length - 1];
       var initiator = request?.initiator;
-      var websiteName = new URL(initiator).hostname
-      console.log('WEbsiteName ', websiteName)
+      var websiteName = new URL(initiator).hostname;
+      console.log("WEbsiteName ", websiteName);
 
-      hostName = websiteName + " | " + hostName + " (ds: " + datasetName + ")"
+      hostName = websiteName + " | " + hostName + " (ds: " + datasetName + ")";
 
-      var isPayload = {} 
-      isPayload[epocheDate] = {}
-      isPayload[epocheDate]['url'] = request.url;
-      isPayload[epocheDate]['payload'] = payload;
-      isPayload[epocheDate]['datetime'] = Date.now();
-      isPayload[epocheDate]['website'] = websiteName;
+      var isPayload = {};
+      isPayload[epocheDate] = {};
+      isPayload[epocheDate]["url"] = request.url;
+      isPayload[epocheDate]["payload"] = payload;
+      isPayload[epocheDate]["datetime"] = Date.now();
+      isPayload[epocheDate]["website"] = websiteName;
 
-      chrome.storage.local.get(null, function(originalPayload){
-        if(Object.keys(originalPayload).includes(hostName)) {
+      chrome.storage.local.get(null, function (originalPayload) {
+        if (Object.keys(originalPayload).includes(hostName)) {
           originalPayload[hostName].push(isPayload);
         } else {
           originalPayload[hostName] = [];
           originalPayload[hostName].push(isPayload);
         }
-        chrome.storage.local.set(originalPayload, async function(){
+        chrome.storage.local.set(originalPayload, async function () {
           //Update icon to show something is saved
-          chrome.action.setIcon({ path:  "../images/activeImg/cloud-48.png" });
+          chrome.action.setIcon({ path: "../images/activeImg/cloud-48.png" });
           await delay(CONFIG.RECORD_TIMER);
-          chrome.action.setIcon({ path:  "../images/cloud/cloud-48.png" });
+          chrome.action.setIcon({ path: "../images/cloud/cloud-48.png" });
         });
       });
-    }
-    else if(request.method == 'POST' 
-    && request.url.indexOf('evergage.com/api2/event/') > -1) 
-  {
-    console.log('POST Request ' , request)
-    var url = new URL(decodeURIComponent(request.url));
-    console.log('Post method - get payload ', JSON.parse(request?.requestBody?.formData?.event?.[0]))
-    var decodedJson = JSON.parse(request?.requestBody?.formData?.event?.[0]);
-    var epocheDate = Date.now().toString();
-    var payload = decodedJson;
-    var hostName = url.hostname;
-    hostName = hostName.split('.')[0];
-    var datasetName = url.pathname.split('/')[url.pathname.split('/').length-1]; 
-    var initiator = request?.initiator;
-    var websiteName = new URL(initiator).hostname
-    console.log('WEbsiteName ', websiteName)
+    } else if (
+      request.method == "POST" &&
+      request.url.indexOf("evergage.com/api2/event/") > -1
+    ) {
+      console.log("POST Request ", request);
+      var url = new URL(decodeURIComponent(request.url));
+      console.log(
+        "Post method - get payload ",
+        JSON.parse(request?.requestBody?.formData?.event?.[0])
+      );
+      var decodedJson = JSON.parse(request?.requestBody?.formData?.event?.[0]);
+      var epocheDate = Date.now().toString();
+      var payload = decodedJson;
+      var hostName = url.hostname;
+      hostName = hostName.split(".")[0];
+      var datasetName =
+        url.pathname.split("/")[url.pathname.split("/").length - 1];
+      var initiator = request?.initiator;
+      var websiteName = new URL(initiator).hostname;
+      console.log("WEbsiteName ", websiteName);
 
-    hostName = websiteName + " | " + hostName + " (ds: " + datasetName + ")"
+      hostName = websiteName + " | " + hostName + " (ds: " + datasetName + ")";
 
-    var isPayload = {} 
-    isPayload[epocheDate] = {}
-    isPayload[epocheDate]['url'] = request.url;
-    isPayload[epocheDate]['payload'] = payload;
-    isPayload[epocheDate]['datetime'] = Date.now();
-    isPayload[epocheDate]['website'] = websiteName;
+      var isPayload = {};
+      isPayload[epocheDate] = {};
+      isPayload[epocheDate]["url"] = request.url;
+      isPayload[epocheDate]["payload"] = payload;
+      isPayload[epocheDate]["datetime"] = Date.now();
+      isPayload[epocheDate]["website"] = websiteName;
 
-    chrome.storage.local.get(null, function(originalPayload){
-      if(Object.keys(originalPayload).includes(hostName)) {
-        originalPayload[hostName].push(isPayload);
-      } else {
-        originalPayload[hostName] = [];
-        originalPayload[hostName].push(isPayload);
-      }
-      chrome.storage.local.set(originalPayload, async function(){
-        //Update icon to show something is saved
-        chrome.action.setIcon({ path:  "../images/activeImg/cloud-48.png" });
-        await delay(CONFIG.RECORD_TIMER);
-        chrome.action.setIcon({ path:  "../images/cloud/cloud-48.png" });
+      chrome.storage.local.get(null, function (originalPayload) {
+        if (Object.keys(originalPayload).includes(hostName)) {
+          originalPayload[hostName].push(isPayload);
+        } else {
+          originalPayload[hostName] = [];
+          originalPayload[hostName].push(isPayload);
+        }
+        chrome.storage.local.set(originalPayload, async function () {
+          //Update icon to show something is saved
+          chrome.action.setIcon({ path: "../images/activeImg/cloud-48.png" });
+          await delay(CONFIG.RECORD_TIMER);
+          chrome.action.setIcon({ path: "../images/cloud/cloud-48.png" });
+        });
       });
-    });
-  } //if ends here 
-    
+    } //if ends here
   },
-  {urls: [ "<all_urls>"]},
-  ['requestBody']
-); 
+  { urls: ["<all_urls>"] },
+  ["requestBody"]
+);
+
+// // Enable if switched on
+
+// const HEADERS_TO_STRIP_LOWERCASE = [
+//   "content-security-policy",
+//   "x-frame-options",
+// ];
+
+// chrome.webRequest.onBeforeRequest.addListener(
+//   (details) => {
+//     console.log("Removing headers for ", details);
+//     ({
+//       responseHeaders: details.responseHeaders.filter(
+//         (header) =>
+//           !HEADERS_TO_STRIP_LOWERCASE.includes(header.name.toLowerCase())
+//       ),
+//     });
+//   },
+//   {
+//     urls: ["<all_urls>"],
+//   },
+//   ["blocking", "responseHeaders", "extraHeaders"]
+// );
--- popup.js
+++ popup.js
@@ -1,55 +1,53 @@
 //Global Variable
-var jsonToClipboard = '';
+var jsonToClipboard = "";
 
-document.getElementById('BtnRefresh').addEventListener('click', init);
-document.getElementById('BtnClear').addEventListener('click', clearAll);
+document.getElementById("BtnRefresh").addEventListener("click", init);
+document.getElementById("BtnClear").addEventListener("click", clearAll);
 
-var txtAreaField = document.getElementById('atob');
-txtAreaField.addEventListener('input', function() {
-  
+var txtAreaField = document.getElementById("atob");
+txtAreaField.addEventListener("input", function () {
   var encodedString = txtAreaField.value;
-  try { 
-    var decodedString = atob(encodedString);
+  try {
+    var decodedString = decodeURIComponent(escape(atob(encodedString)));
     var decodedJson = JSON.parse(decodedString);
 
     generateJsonViewer(decodedJson);
-  }catch(e) {
-    var errorDiv =  $('#json').empty().html('<font color="red">Error: Invalid Base64 text</font>')
+  } catch (e) {
+    var errorDiv = $("#json")
+      .empty()
+      .html('<font color="red">Error: Invalid Base64 text</font>');
   }
 });
 
-
 async function getData() {
-  return new Promise( (resolve, reject) => {
-    chrome.storage.local.get(null, function(items){
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.get(null, function (items) {
       resolve(items);
     });
   });
 }
 
 async function init() {
-
   //Reset UI
-  document.getElementById('jsonImg').hidden = false;
-  document.getElementById('json').hidden = true; 
-  document.getElementById('bd-clipboard').hidden = true; 
-  document.getElementById('json').innerHTML = '';
-  document.getElementById('atob').value = '';
+  document.getElementById("jsonImg").hidden = false;
+  document.getElementById("json").hidden = true;
+  document.getElementById("bd-clipboard").hidden = true;
+  document.getElementById("json").innerHTML = "";
+  document.getElementById("atob").value = "";
 
   var o = await getData();
-  
+
   var listOfSites = Object.keys(o);
 
-  document.getElementById('accordionExample').innerHTML = ''; //Clear for first time
-  if(listOfSites.length > 0) {
-    for(l in listOfSites) {
+  document.getElementById("accordionExample").innerHTML = ""; //Clear for first time
+  if (listOfSites.length > 0) {
+    for (l in listOfSites) {
       var accordionHtml = generateAccordianItems(l, listOfSites[l]);
-      document.getElementById('accordionExample').appendChild(accordionHtml);
+      document.getElementById("accordionExample").appendChild(accordionHtml);
     }
   } else {
-    //No sites captured 
-    document.getElementById('accordionExample').innerHTML = 
-    `
+    //No sites captured
+    document.getElementById("accordionExample").innerHTML = `
     <div class="alert alert-info" role="alert">
       No sites captured yet.
     </div>
@@ -57,18 +55,29 @@ async function init() {
   }
 
   function generateAccordianItems(itemNumber, hostName) {
-
-    var tableItems = '';
+    var tableItems = "";
     var savedItems = o[hostName];
 
     var htmlBulletCounter = 1;
-    for(var item = savedItems.length-1; item >= 0 ; item--){
+    for (var item = savedItems.length - 1; item >= 0; item--) {
       var key = Object.keys(savedItems[item])[0];
       var tmpDate = new Date(Number(key));
       var dateSince = moment(tmpDate).fromNow();
-      var savedUrl = new URL(decodeURIComponent(savedItems[item][key]['url'] ));
-      var encodedString = btoa(JSON.stringify(savedItems[item][key]['payload']));
-      var itemClass = (htmlBulletCounter <= 5 ? 'ck-item-show':'ck-item-hide');
+      var savedUrl = new URL(decodeURIComponent(escape(savedItems[item][key]["url"]))); // decodeURIComponent(escape(atob(base64)))
+      // Method 1
+      // var encodedString = btoa(JSON.stringify(savedItems[item][key]['payload']));
+      // Method 2 
+      // you’re --> youâre
+      var payload = savedItems[item][key]["payload"];
+      var payloadStr = JSON.stringify(payload);
+      var encodedString = btoa(unescape(encodeURIComponent(payloadStr)));
+      // Method 3
+      // const encoder = new TextEncoder();
+      // const str = JSON.stringify(savedItems[item][key]['payload']);
+      // const data = encoder.encode(str);
+      // const encodedString = btoa(String.fromCharCode.apply(null, data));
+
+      var itemClass = htmlBulletCounter <= 5 ? "ck-item-show" : "ck-item-hide";
       tableItems += `<div class="${itemClass} ck-itemNumber-${htmlBulletCounter} accordion-body text-start" style="padding-top: 0px !important; padding-bottom: 0px !important" >${htmlBulletCounter}) 
       ${dateSince}
       <span
@@ -80,13 +89,15 @@ async function init() {
           >
           <sup><img height="12px" width="12px" src="./images/clock.png"/></sup>
       </span>
-      <a class="btn btn-link ck-viewEncodedString" encodedString="${encodedString}" accordionId="${'collapse_'+itemNumber}" href="#json">View</a></div>`;
+      <a class="btn btn-link ck-viewEncodedString" encodedString="${encodedString}" accordionId="${
+        "collapse_" + itemNumber
+      }" href="#json">View</a></div>`;
       htmlBulletCounter++;
     }
 
     var showMoreDiv = `<a class="ck-showMore" id="ck-showMore">Show All</a>`;
-    showMoreDiv = (savedItems.length > 5 ? showMoreDiv : '');
-    var tmpHtml  = `
+    showMoreDiv = savedItems.length > 5 ? showMoreDiv : "";
+    var tmpHtml = `
       <div class="accordion" id="heading_${itemNumber}">
       <div class="accordion-item mb-3">
             <h2 class="accordion-header">
@@ -113,76 +124,72 @@ async function init() {
         </div>
     `;
 
-    var accordionHtml = document.createElement('div');
-    accordionHtml.className = 'accordion-item';
-    accordionHtml.innerHTML= tmpHtml;
+    var accordionHtml = document.createElement("div");
+    accordionHtml.className = "accordion-item";
+    accordionHtml.innerHTML = tmpHtml;
 
     return accordionHtml;
   }
 
-  document.querySelectorAll('.ck-showMore').forEach(function(item){
-    item.addEventListener('click', function(){
-      $('.ck-item-hide').addClass('ck-item-show').removeClass('ck-item-hide');
-      $('.ck-showMore').addClass('ck-item-hide');
+  document.querySelectorAll(".ck-showMore").forEach(function (item) {
+    item.addEventListener("click", function () {
+      $(".ck-item-hide").addClass("ck-item-show").removeClass("ck-item-hide");
+      $(".ck-showMore").addClass("ck-item-hide");
     });
   });
 
-  document.querySelectorAll('.ck-encodedString').forEach(function(item) {
-    item.addEventListener('click', function () {
+  document.querySelectorAll(".ck-encodedString").forEach(function (item) {
+    item.addEventListener("click", function () {
       navigator.clipboard.writeText(item.innerHTML);
-      document.getElementById('atob').innerHTML = item.innerHTML;
-      var event = new Event('input');
-      document.getElementById('atob').dispatchEvent(event);
+      document.getElementById("atob").innerHTML = item.innerHTML;
+      var event = new Event("input");
+      document.getElementById("atob").dispatchEvent(event);
     });
   });
 
-  document.querySelectorAll('.ck-viewEncodedString').forEach(function(item) {
-    item.addEventListener('click', function () {
-      $('#atob').val(item.getAttribute('encodedstring'));
-      var event = new Event('input');
-      document.getElementById('atob').dispatchEvent(event);
-      // hide accordion by simulating click action 
-      var event = new Event('click');
-      var accordionId = $(this).attr('accordionId');
-      document.getElementById('atob').dispatchEvent(event);
+  document.querySelectorAll(".ck-viewEncodedString").forEach(function (item) {
+    item.addEventListener("click", function () {
+      $("#atob").val(item.getAttribute("encodedstring"));
+      var event = new Event("input");
+      document.getElementById("atob").dispatchEvent(event);
+      // hide accordion by simulating click action
+      var event = new Event("click");
+      var accordionId = $(this).attr("accordionId");
+      document.getElementById("atob").dispatchEvent(event);
     });
   });
 
   //$('.ck-item-show').tooltip({show: {effect:"none", delay:0 }});
-
 }
 
 async function clearAll() {
-  return new Promise( (resolve, reject) => {
-    chrome.storage.local.clear(function(){
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.clear(function () {
       init();
       resolve(true);
     });
-  });  
+  });
 }
 init();
 
-
-
-//Json viewer 
-function generateJsonViewer(jsonObj) { 
+//Json viewer
+function generateJsonViewer(jsonObj) {
   jsonToClipboard = jsonObj;
   var jsonViewer = new JSONViewer();
 
-  document.querySelector("#json").hidden = false; 
-  document.querySelector('#bd-clipboard').hidden = false;
-  document.querySelector("#jsonImg").hidden = true; 
-  document.querySelector("#json").innerHTML = '';
+  document.querySelector("#json").hidden = false;
+  document.querySelector("#bd-clipboard").hidden = false;
+  document.querySelector("#jsonImg").hidden = true;
+  document.querySelector("#json").innerHTML = "";
   document.querySelector("#json").appendChild(jsonViewer.getContainer());
 
   var textarea = document.querySelector("atob");
   // textarea value to JSON object
-  var setJSON = function() {
+  var setJSON = function () {
     try {
       // var value = textarea.value;
       // jsonObj = JSON.parse(value);
-    }
-    catch (err) {
+    } catch (err) {
       alert(err);
     }
   };
@@ -190,24 +197,32 @@ function generateJsonViewer(jsonObj) {
   // load default value
   setJSON();
 
-  jsonViewer.showJSON(jsonObj); //Show all 
-  // jsonViewer.showJSON(jsonObj, null, 1); //Collapse to level 1  
+  jsonViewer.showJSON(jsonObj); //Show all
+  // jsonViewer.showJSON(jsonObj, null, 1); //Collapse to level 1
   // jsonViewer.showJSON(jsonObj, 1); // Show only level 1
 }
 
 // https://getbootstrap.com/docs/4.0/components/tooltips/#methods
 $(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
+  $('[data-toggle="tooltip"]').tooltip();
+});
 
-
-$('.btn-clipboard').on('click', function() { 
-  $(".btn-clipboard-tooltip-span").attr('data-bs-original-title', 'Copied!')
-  //.tooltip('fixTitle')
-  .tooltip('show');
+$(".btn-clipboard").on("click", function () {
+  $(".btn-clipboard-tooltip-span")
+    .attr("data-bs-original-title", "Copied!")
+    //.tooltip('fixTitle')
+    .tooltip("show");
 
   navigator.clipboard.writeText(JSON.stringify(jsonToClipboard));
 });
-$('.btn-clipboard').on('mouseover', function() { 
-  $(".btn-clipboard-tooltip-span").attr('data-bs-original-title', 'Copy to clipboard!');
+$(".btn-clipboard").on("mouseover", function () {
+  $(".btn-clipboard-tooltip-span").attr(
+    "data-bs-original-title",
+    "Copy to clipboard!"
+  );
 });
+
+// $("#toggleXFrame").on("click", function () {
+//   console.log("toggle x frame");
+//   console.log($(".toggleXFrame").attr("selected"));
+// });
